382ce89ab38e231a0377e46e9403031416d2cfa6
mehrshad <mehrshadkhakifirooz@gmail.com>
2025-11-28 17:38:51 +0330

    Add project files.

---
+++ Library Manegment Domain/Entities/Books/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace Library_Manegment_Domain.Entities.Books
+{
+    public class Book
+    {
+        public int Id { get; private set; }
+        public string Titlt { get; private set; }
+        public string Author { get; private set; }
+        public string Language { get; private set; }
+        public byte[] Image { get; private set; }
+
+        public Book(string titlt, string author, string language, byte[] image)
+        {
+            Titlt = titlt;
+            Author = author;
+            Language = language;
+            Image = image;
+        }
+    }
+}
