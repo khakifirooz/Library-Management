5b952798855566905b301f436765150540e5f266
mehrshad <mehrshadkhakifirooz@gmail.com>
2025-11-29 20:35:32 +0330

    create custome data type in framework=>operation result

---
+++ FrameworkApplication/OperationResult.cs
@@ -0,0 +1,26 @@
+﻿namespace FrameworkApplication
+{
+    public class OperationResult
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public OperationResult()
+        {
+            Success = false;
+        }
+
+        public OperationResult Succeded(string message = "عملیات با موفقیت انجام شد.")
+        {
+            Success = true;
+            Message = message;
+            return this;
+        }
+        public OperationResult Failed(string message = "لطفا دوباره تلاش کنید")
+        {
+            Success = false;
+            Message = message;
+            return this;
+        }
+
+    }
+}
