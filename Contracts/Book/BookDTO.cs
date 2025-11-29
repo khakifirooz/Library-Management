8f3a8dc2a66f2796f159ec55e0b31900695b1f0d
mehrshad <mehrshadkhakifirooz@gmail.com>
2025-11-29 19:59:05 +0330

    Design Application Layer

---
+++ Contracts/Book/BookDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Contracts.Book
+{
+    public class BookDTO  // data transform object
+    {
+        public int Id { get; set; }
+        public string Titlt { get; private set; }
+        public string Author { get; private set; }
+        public string Language { get; private set; }
+        public byte[] Image { get; private set; }
+    }
+}
---
+++ Contracts/Book/IBookService.cs
@@ -0,0 +1,11 @@
+﻿namespace Contracts.Book
+{
+    public interface IBookService
+    {
+        Task CreateAsync(BookDTO command);
+        Task UpdateAsync(BookDTO command);
+        Task DeleteAsync(int id);
+        Task<BookDTO> GetByIdAsync(int id);
+        Task<List<BookDTO>> GetAllAsync();
+    }
+}
