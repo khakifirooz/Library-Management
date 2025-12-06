70fa36c56512d7ce06bb9b5f74212af6817dfeda
mehrshad <mehrshadkhakifirooz@gmail.com>
2025-12-06 13:31:08 +0330

    shamsi to miladi and miladi date to shamsi class added in framework project

---
+++ FrameworkApplication/ConverDate.cs
@@ -0,0 +1,19 @@
+﻿using MD.PersianDateTime;
+
+namespace FrameworkApplication
+{
+    public static class ConverDate  // we use static becaus we want to use extension method
+    {
+        public static DateTime ConvertShamsiDateToMilady(this string date)
+        {
+            PersianDateTime persianDate = PersianDateTime.Parse(date);
+            return persianDate.ToDateTime();
+        }
+
+        public static string ConvertMiladiDateToShamsi(this DateTime date)
+        {
+            PersianDateTime persianDate = new PersianDateTime(date);
+            return persianDate.ToString();
+        }
+    }
+}
