25dc9197814ac8f87104b85705887c194e8f66c8
mehrshad <mehrshadkhakifirooz@gmail.com>
2026-06-17 15:48:42 +0330

    management App update 1

--- LibraryManagementWindowsForm/LoginForm.Designer.cs
+++ LibraryManagementWindowsForm/LoginForm.Designer.cs
@@ -41,48 +41,61 @@ namespace LibraryManagementWindowsForm
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(351, 91);
+            label1.BackColor = SystemColors.ActiveCaptionText;
+            label1.Font = new Font("B Nazanin", 12F, FontStyle.Regular, GraphicsUnit.Point, 178);
+            label1.ForeColor = Color.Chartreuse;
+            label1.Location = new Point(342, 101);
             label1.Name = "label1";
-            label1.Size = new Size(107, 20);
+            label1.Size = new Size(103, 29);
             label1.TabIndex = 0;
             label1.Text = "لطفا وارد شوید";
             label1.TextAlign = ContentAlignment.TopCenter;
+            label1.UseWaitCursor = true;
             // 
             // btn1
             // 
-            btn1.Location = new Point(351, 275);
+            btn1.BackColor = Color.LightSalmon;
+            btn1.Font = new Font("B Nazanin", 12F, FontStyle.Regular, GraphicsUnit.Point, 178);
+            btn1.ForeColor = Color.Black;
+            btn1.Location = new Point(351, 259);
             btn1.Name = "btn1";
-            btn1.Size = new Size(94, 29);
+            btn1.Size = new Size(94, 43);
             btn1.TabIndex = 1;
-            btn1.Text = "ورود";
-            btn1.UseVisualStyleBackColor = true;
-            btn1.Click += this.btn1_Click;
+            btn1.Text = "ورود ادمین";
+            btn1.UseVisualStyleBackColor = false;
+            btn1.Click += btn1_Click;
             // 
             // txt_user
             // 
-            txt_user.Location = new Point(338, 153);
+            txt_user.Location = new Point(300, 153);
             txt_user.Name = "txt_user";
-            txt_user.Size = new Size(125, 27);
+            txt_user.Size = new Size(204, 27);
             txt_user.TabIndex = 2;
+            txt_user.UseWaitCursor = true;
             // 
             // txt_pass
             // 
-            txt_pass.Location = new Point(338, 212);
+            txt_pass.Location = new Point(300, 212);
             txt_pass.Name = "txt_pass";
-            txt_pass.Size = new Size(125, 27);
+            txt_pass.Size = new Size(204, 27);
             txt_pass.TabIndex = 3;
+            txt_pass.UseWaitCursor = true;
             // 
             // LoginForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Desktop;
             ClientSize = new Size(800, 450);
             Controls.Add(txt_pass);
             Controls.Add(txt_user);
             Controls.Add(btn1);
             Controls.Add(label1);
+            ForeColor = Color.LawnGreen;
             Name = "LoginForm";
-            Text = "LoginForm";
+            RightToLeftLayout = true;
+            Text = "فرم ورود";
+            UseWaitCursor = true;
             Load += LoginForm_Load;
             ResumeLayout(false);
             PerformLayout();
